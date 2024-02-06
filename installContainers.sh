527a65a9e097610f90ba796a4646092a9f632c19
Kaento <kaento.juank@espoch.edu.ec>
2024-02-06 12:48:11 -0500

    !

--- installContainers.sh
+++ installContainers.sh
@@ -1,13 +1,6 @@
-docker run -d -p 3307:3306 --name mysql -v /home/dataproc/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 mysql:5.7
+docker run -d -p 3307:3306 --name mysql -v ./mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 mysql:5.7
 
-docker run -d -p 80:80 \
--v /home/dataproc/php/:/var/www/html \
--v /home/dataproc/apache2/:/etc/apache2 \
---name php --link mysql php:7.0-apache
-
-docker cp php:/etc/apache2 ./apache2
-
-docker exec -it php /bin/bash
+docker run -d -p 80:80 -v ./phpconf/:/var/www/html -v /home/dataproc/apache2/:/etc/apache2 --name php --link mysql php:7.0-apache
 
 
 
